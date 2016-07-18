98b6c3ae8c1d1ee46c737a9619270cf7c5e7044e
hounenhounen <kawahara@kawahara-no-MacBook-Air-8.local>
2016-07-18 22:14:30 +0900

    PushTimerfirst

---
+++ Assets/ReceiveTest.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using NCMB;
+
+public class ReceiveTest : MonoBehaviour {
+	private static bool _isInitialized = false;
+
+	/// <summary>
+	///イベントリスナーの登録
+	/// </summary>
+	void OnEnable()
+	{
+		NCMBManager.onRegistration += OnRegistration;
+		NCMBManager.onNotificationReceived += OnNotificationReceived;
+	}
+
+	/// <summary>
+	///イベントリスナーの削除
+	/// </summary>
+	void OnDisable ()
+	{
+		NCMBManager.onRegistration -= OnRegistration;
+		NCMBManager.onNotificationReceived -= OnNotificationReceived;
+	}
+
+	/// <summary>
+	///端末登録後のイベント
+	/// </summary>
+	void OnRegistration (string errorMessage)
+	{
+		if (errorMessage == null) {
+			Debug.Log ("OnRegistrationSucceeded");
+		} else {
+			Debug.Log ("OnRegistrationFailed:" + errorMessage);
+		}
+	}
+
+	/// <summary>
+	///メッセージ受信後のイベント
+	/// </summary>
+	void OnNotificationReceived(NCMBPushPayload payload)
+	{
+		
+		// クラスのNCMBObjectを作成
+		NCMBObject PushTime = new NCMBObject("PushTime");
+		// オブジェクトに値を設定
+		PushTime["SendTime"] = SendPush.SendTime;
+		System.DateTime ReceiveTime = System.DateTime.UtcNow;
+		PushTime["ReceiveTime"] = ReceiveTime;
+		TimeSpan ts = ReceiveTime - SendPush.SendTime;
+		PushTime ["DeliveryTime"] = ts.TotalSeconds;
+		// データストアへの登録
+		PushTime.SaveAsync();
+		Debug.Log("OnNotificationReceived");
+	}
+
+	/// <summary>
+	///シーンを跨いでGameObjectを利用する設定
+	/// </summary>
+	public virtual void Awake ()
+	{
+		if (!_isInitialized) {
+			_isInitialized = true;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SendPush.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+public class SendPush : MonoBehaviour {
+	public static System.DateTime SendTime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PushSend(){
+		NCMBPush PushSend = new NCMBPush ();
+		PushSend.Add ("title","TestTest");
+		PushSend.Add ("message","clientTest");
+		PushSend.PushToAndroid = true;
+		PushSend.SendPush ((NCMBException e) => { 
+				if (e != null) {
+					UnityEngine.Debug.Log ("失敗: " + e.ErrorMessage);
+				} else {
+					SendTime = System.DateTime.UtcNow;
+					UnityEngine.Debug.Log ("成功" + SendTime);
+				}
+			}
+		);
+
+	}
+}
